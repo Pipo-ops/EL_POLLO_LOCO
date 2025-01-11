4862e2cbc7ee8a9d7ca18238924cb86dddc29944
Schuster Phillip <schusterphillip63@gmail.com>
2025-01-11 18:45:53 +0100

    integrating models

--- js/game.js
+++ js/game.js
@@ -1,14 +1,10 @@
 let canvas;
-let ctx;
-let character = new Image();
+let world;
 
 
 function init(){
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
-
-    character.src = '..img/2_character_pepe/1_idle/idle/I-1.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);
-}
+    
+}  
